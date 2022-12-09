87cdb5e018c42f1e919ffac184477a75c8c5019c
tomisinoruns <orunsoluoluwatomisin@gmail.com>
2022-12-08 17:15:04 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio>
+/**
+ * main- Entry
+ * Return: 0
+ */
+int main(0)
+{
+char charType;
+int intType;
+long longType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+return (0);
+}
