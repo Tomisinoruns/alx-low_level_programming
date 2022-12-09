4d54a3ca1b6dd121c0403da291d4768b95ac46ad
tomisinoruns <orunsoluoluwatomisin@gmail.com>
2022-12-08 16:24:30 -0800

    inputing words

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-char string[] = "\"Programming is like building a multilingual puzzle \n ";
-
-puts(string);
-
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
