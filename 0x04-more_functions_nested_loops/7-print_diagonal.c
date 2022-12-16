fdb73e315324ffbd5f68b8958da8f307e0c36e9b
tomisinoruns <orunsoluoluwatomisin@gmail.com>
2022-12-16 15:23:17 -0800

    more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,7 @@ else
 {
 int i, j;
 for (i = 0; i < n; i++)
-(
+{
 for (j = 0; j < n; j++)
 {
 if (j == i)
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  *main- prints the numbers from 1 to 100,
- *followed by a line
+ *followed by a new line
  *but for the multiples of three prints Fizz instead of the number
  *and for the multiples of five prints Buzz
  *Return: 0
