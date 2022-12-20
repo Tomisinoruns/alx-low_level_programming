feddbe32e067e2c4b647a0d80ecac0b5a2ac8f64
tomisinoruns <orunsoluoluwatomisin@gmail.com>
2022-12-19 17:42:13 -0800

    change directory error

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_rev- prints a string in reverse,
+ *followed by a new line
+ *@str: string to reverse
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+int i, j, n;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+n = i;
+for (j = n - 1; j >= 0; j--)
+{
+_putchar('\n');
+}
+}
